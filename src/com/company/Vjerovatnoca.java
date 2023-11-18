0f7dfc217011102a571a50461de4075e55aa2d84
lanamal <lmalinov1@etf.unsa.ba>
2023-11-18 21:38:59 +0100

    zad3

--- src/com/company/Vjerovatnoca.java
+++ src/com/company/Vjerovatnoca.java
@@ -3,50 +3,36 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Collections;
-import java.util.Iterator;
+import static java.lang.Math.*;
+import static java.util.Collections.*;
 
 public class Vjerovatnoca {
 
     public static void main(String[] args) {
-        List<Double> niz=new ArrayList();
+        List<Integer> lista=new ArrayList<>();
         Scanner ulaz=new Scanner(System.in);
-
-        while(true) {
-            System.out.println("Unesite broj ili rijec-stop za kraj unosa: ");
-            String unos=ulaz.nextLine();
-            if (unos.equalsIgnoreCase("stop")) {
-                if (niz.isEmpty()) {
-                    System.out.println("Niste unijeli broj!");
-                } else {
-                    double min=(Double)Collections.min(niz);
-                    double max=(Double)Collections.max(niz);
-                    double suma=0.0;
-
-                    double broj1;
-                    for(Iterator it=niz.iterator();it.hasNext();suma+=broj1)
-                        broj1=(Double)it.next();
-
-                    double srednjaVri=suma/(double)niz.size();
-
-                    double suma2=0.0;
-                    double broj2;
-                    for(Iterator it=niz.iterator();it.hasNext();suma2+=Math.pow(broj2-srednjaVri, 2.0)) {
-                        broj2=(Double)it.next();
-                    }
-
-                    double standardnaDev = Math.sqrt(suma2/(double)niz.size());
-                    System.out.println("Minimum iznosi " +min);
-                    System.out.println("Maksimum iznosi " +max);
-                    System.out.println("Srednja vrijednost iznosi " +srednjaVri);
-                    System.out.println("Standardna devijacija iznosi " +standardnaDev);
-                }
-                return;
+        System.out.println("Unesite brojeve (stop za prekid, slovo za nastavak):");
+        while(ulaz.hasNext()) {
+            if(ulaz.hasNextInt()) {
+                int broj=ulaz.nextInt();
+                lista.add(broj);
             }
-            try {
-                niz.add(Double.parseDouble(unos));
-            } catch (NumberFormatException var17) {
-                System.out.println("Niste unijeli ispravan broj! Molim vas pokušajte ponovno.");
+            else {
+                String string=ulaz.next();
+                if(string.equals("stop"))
+                    break;
             }
         }
+
+        int maksimum= (int) max(lista), minimum=(int) min(lista), suma=0;
+        double mean;
+        for(int i=0; i<lista.size(); i++)
+            suma+=lista.get(i);
+        mean=(double)suma/lista.size();
+        double sumaDev=0;
+        for(int i=0; i<lista.size(); i++)
+            sumaDev+=pow(lista.get(i)-mean,2);
+        double standDev=(double)sqrt(sumaDev/lista.size());
+        System.out.println("Max = "+maksimum+"\nMin = "+minimum+"\nMean = "+mean+"\nDevijacija = "+standDev);
     }
 }
